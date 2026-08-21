60f4e3a7cdf2c0df745017ab78c995a50e0ea5af
Harsh Kumar <harshdev808@gmail.com>
2026-08-21 18:14:20 +0530

    Sorting and Searching: 9. Missing Sum Coin

--- cses speedrun/src/App.java
+++ cses speedrun/src/App.java
@@ -80,70 +80,18 @@ public class App {
         out.flush();
     }
 
-    /**
-     * Out of any 2 movies we will always choose the movie that ends first
-     */
     private static void solve() {
         int n = in.nextInt();
-        long[] a = new long[n];
-
-        for(int i = 0; i < n; i++) {
-            a[i] = in.nextLong();
-        }
-
+        int[] a = in.nextIntArray(n);
         Arrays.sort(a);
 
-        
-        long[] ps = new long[n];
-        ps[0] = a[0];
-        
-        for(int i = 1; i < n ; i++) {
-            ps[i] = a[i] + ps[i - 1];
-        }
-        
-        long l = 0;
-        long r = a[n - 1];
-
-        while (l <= r) {
-            long mid = (l + r) / 2;
-            long c_mid = calculate_cost(n, ps, mid, a);
-            long c_l = calculate_cost(n, ps, mid - 1, a);
-            long c_r = calculate_cost(n, ps, mid + 1, a);
-
-            if(c_l >= c_mid && c_mid <= c_r) {
-                out.println(c_mid);
-                return;
-            } else if (c_l >= c_mid) {
-                l = mid + 1;
-            } else {
-                r = mid;
+        long ans = 1;
+        for(int c: a) {
+            if (c > ans) {
+                break;
             }
+            ans = ans + c;
         }
-    }
-    
-    private static long calculate_cost(int n, long[] ps, long mid, long[] a) {
-        int i_mid = floorBinarySearch(a, mid);
-        long c_mid = 0;
-        if(i_mid >= 0) {
-            c_mid += mid*(i_mid + 1) - ps[i_mid];
-            c_mid += ps[n - 1] - ps[i_mid] - (n - 1 - i_mid)*mid;
-        } else {
-            c_mid += ps[n - 1] - (n - 1 - i_mid)*mid;
-        }
-        return c_mid;
-    }
-
-    static int floorBinarySearch(long[] a, long t) {
-        int l = 0;
-        int r = a.length - 1;
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (a[mid] <= t) {
-                l = l + 1;
-            } else {
-                r = r - 1;
-            }
-        }
-        return r;
+        out.println(ans);
     }
 }
